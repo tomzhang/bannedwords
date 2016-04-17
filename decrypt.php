6482ebb2a757f0f5727b62a0213915c8e2971cda
spetacular <uexam@163.com>
2016-04-17 09:44:52 +0800

    短信专用违禁词

--- decrypt.php
+++ decrypt.php
@@ -1,8 +1,17 @@
 <?php
-$lines = file('./pubbannedwords.txt');
+/**
+* Usage: decrpty.php filename
+* /usr/bin/php decrypt.php raw_sms_banned_words.txt
+* URL:   https://github.com/spetacular/bannedwords
+*/
+if ($argc != 2) { 
+	die("Usage: /usr/bin/php decrypt.php filename\n"); 
+} 
+$filename = $argv[1];
+$lines = file('./'.$filename);
 $newsLines = array();
 foreach ($lines as $rawword) {
     $newsLines[] = base64_decode(trim($rawword));
 }
-file_put_contents('./rawbannedwords_out.txt', implode("\n", $newsLines));
+file_put_contents('./'.$filename.'.out.txt', implode("\n", $newsLines));
 echo "done\n";
--- encrypt.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lines = file('./rawbannedwords.txt');
-$newsLines = array();
-foreach ($lines as $rawword) {
-    $newsLines[] = base64_encode($rawword);
-}
-file_put_contents('./pubbannedwords.txt', implode("\n", $newsLines));
-echo "done\n";
