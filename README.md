641602764e70708e07fc4bf3f761a4770e06671d
spetacular <uexam@163.com>
2016-04-17 10:02:26 +0800

    change readme

--- README.md
+++ README.md
@@ -10,8 +10,18 @@
 
 #如何使用
 发布违禁词本身就是违禁的。。。所以生成的词库都用base64编码。
-大家使用时，可以用 decrypt.php 解码，生成的rawbannedwords_out.txt文件里即是违禁词。
+大家使用时，可以用 decrypt.php filename 解码，例如：
+
+    /usr/bin/php decrypt.php pub_sms_banned_words.txt
+
+pub_sms_banned_words.txt.out.txt文件里即是违禁词。
+
 或者使用 [在线服务](http://tool.chinaz.com/tools/base64.aspx) 解码，将pubbadwords.txt的内容粘贴在右边框里，点击“Base64解密”，违禁词就出现在左边框里。
 
+免责声明
+所有词库均来自网络，或利用爬虫抓取公开数据得到。
+使用者不得将此词库用于非法目的。
+
 #ChangeLog
+2016-04-17 发布短信专用违禁词库，共2696条
 2016-04-16 发布第一份违禁词库，共1673条。
--- decrypt.php
+++ decrypt.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Usage: decrpty.php filename
-* /usr/bin/php decrypt.php raw_sms_banned_words.txt
+* /usr/bin/php decrypt.php pub_sms_banned_words.txt
 * URL:   https://github.com/spetacular/bannedwords
 */
 if ($argc != 2) { 
@@ -13,5 +13,5 @@ $newsLines = array();
 foreach ($lines as $rawword) {
     $newsLines[] = base64_decode(trim($rawword));
 }
-file_put_contents('./'.$filename.'.out.txt', implode("\n", $newsLines));
+file_put_contents('./'.$filename.'.out.txt', implode("", $newsLines));
 echo "done\n";
